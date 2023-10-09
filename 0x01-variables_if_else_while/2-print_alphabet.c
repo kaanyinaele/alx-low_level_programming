65f89512dad39f83f6a0fe2caab9a44629c7d2df
kaanyinaele <kaanyinaele@gmail.com>
2023-10-09 13:02:19 -0700

    ssigh

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -7,9 +6,7 @@
 int main(void)
 {
     char c;
-
-    for (c = 'a'; c <= 'z'; c++)
-   	 putchar(c);
+    for (c = 'a'; c <= 'z'; c++) putchar(c);
     putchar('\n');
     return (0);
 }
