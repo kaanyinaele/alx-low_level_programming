3ec7ba5ff345d12bcfa0df4210f5d411bccde069
kaanyinaele <kaanyinaele@gmail.com>
2023-10-09 23:04:57 -0700

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,11 @@
 int main(void)
 {
 	char c;
-	
-	for (c = 'a'; c <= 'z'; c++) putchar(c);
-	for (c = 'A'; c <= 'Z'; c++) putchar(c);
-	putchar('\n');
+
+for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+putchar('\n');
 	return (0);
 }
