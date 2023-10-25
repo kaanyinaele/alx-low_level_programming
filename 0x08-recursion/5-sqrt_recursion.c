5eaff87a74b5943f11f4210b543a953848c16c56
kaanyinaele <kaanyinaele@duck.com>
2023-10-25 10:28:40 -0700

    Tired of commiting

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,32 @@
 #include "main.h"
+
 /**
 * _sqrt_recursion - returns the natural square root of a number
 * @n: number to be used
 *
 * Return: the square root of n
 */
+
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (find_sqrt(n, 0));
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(0, n));
 }
 
 /**
- * find_sqrt - returns the square root of a number using recursion
- * @n: original number
- * @guess: current guess
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @x: squared number
  *
  * Return: the square root of n
  */
 
-int find_sqrt(int n, int guess)
+int _sqrt(int n, int x)
 {
-	if (guess * guess == n)
-		return (guess);
-	if (guess * guess > n)
+	if (n > x / 2)
 		return (-1);
-	return (find_sqrt(n, guess + 1));
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
