7b1dddecf0a3c670010d20609cdcc56dc0a6ebd2
kaanyinaele <kaanyinaele@gmail.com>
2024-01-16 23:47:25 -0800

    more fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/kaanyinaele/alx-low_level_programming/blob/master/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+
+cp /dev/null 101-make_me_win.sh
+
